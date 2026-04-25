3c1f521252f221294c886f3f3681ab9432f4f05f
[blank] <76683471+LobotomyEnjoyer@users.noreply.github.com>
2026-04-25 16:57:40 +0400

    simple pause/resume handler.

--- source/autoclicker.py
+++ source/autoclicker.py
@@ -3,8 +3,8 @@ import keyboard as kb
 import time
 
 gui.PAUSE = 0 # removes pause between clicks.
-
 INTERVAL_LIMIT = 0.001
+
 
 amount = int(input("Enter amount of clicks (0 = infty): "))
 interval = float(input(f"Enter the interval in seconds between clicks (not lower than {INTERVAL_LIMIT}): "))
@@ -12,13 +12,25 @@ interval = float(input(f"Enter the interval in seconds between clicks (not lower
 if(interval < INTERVAL_LIMIT):
     interval = INTERVAL_LIMIT
 
-count = 0
-print("Press K to start.\nPress L to stop.\n")
+
+print("Press K to start.\nPress L to stop.\nPress J to pause/resume.\n")
+
+
 kb.wait("k")
+count = 0
 while True:
     gui.click()
     count += 1
     time.sleep(interval)
 
+    # exit handler
     if kb.is_pressed("l") or (count >= amount and amount != 0):
         break
+
+    # pause handler
+    if kb.is_pressed("j"):
+        print("Paused. Press J to resume.\n\a")
+        time.sleep(0.1)
+        kb.wait("j")
+        time.sleep(0.1)
+        print("Resumed.\n\a")
