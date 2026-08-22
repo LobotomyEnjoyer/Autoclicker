a64a83ddc936f86f7fbd4274783d7cb76acb41c9
[blank] <76683471+LobotomyEnjoyer@users.noreply.github.com>
2026-08-22 14:22:42 +0400

    added handling for negative values + added 2 clicker functions (counting and not counting)

--- source/autoclicker.py
+++ source/autoclicker.py
@@ -13,6 +13,8 @@ INTERVAL_LIMIT = 0.01 # should be changed to OS' actual limit? 0.0158, perhaps?
 while True:
     try:
         amount = int(input("Enter amount of clicks (0 = infty, default = 0): "))
+        if amount < 0:
+            raise(ValueError)
         break
     except Exception:
         print("Using default.")
@@ -22,14 +24,15 @@ while True:
 while True:
     try:
         interval = float(input(f"Enter the interval in seconds (float) between clicks (below {INTERVAL_LIMIT} and 0 are not recommended, default = {INTERVAL_LIMIT}): "))
+        if interval < 0:
+            raise(ValueError)
         break
     except Exception:
         print("Using default.")
         interval = INTERVAL_LIMIT
         break
-count = 0
 
-interval = 0 if interval < 0 else interval
+counter = 0
 
 # An event to put a threaded process on pause
 pause_event = threading.Event()
@@ -43,19 +46,31 @@ def pause_handler():
         print("Resumed.\n\a" if pause_event.is_set() else "Paused.\n\a")
         time.sleep(0.1)
 
-def clicker():
-    global pause_event, count, interval
+def clicker_counted():
+    global pause_event, counter, interval
     if interval != 0:
         while True:
             pause_event.wait()
             mouse.click("left")
-            count += 1
+            counter += 1
             time.sleep(interval)
     else:  
         while True:
             pause_event.wait()
             mouse.click("left")
-            count += 1
+            counter += 1
+
+def clicker_not_counted():
+    global pause_event, interval
+    if interval != 0:
+        while True:
+            pause_event.wait()
+            mouse.click("left")
+            time.sleep(interval)
+    else:  
+        while True:
+            pause_event.wait()
+            mouse.click("left")
 
 print(f"\nPress {key_start.upper()} to start.\nPress {key_stop.upper()} to stop.\nPress {key_pause.upper()} to pause/resume.\n")
 
@@ -63,10 +78,10 @@ kb.wait(key_start)
 print("The program has started.\n")
 
 threading.Thread(target=pause_handler, daemon=True).start() # Pause handler.
-threading.Thread(target=clicker, daemon=True).start() # Clicker handler.
+threading.Thread(target=(clicker_counted if amount != 0 else clicker_not_counted), daemon=True).start() # Clicker handler.
 
 # Main Thread. Exit handler.
 while True:
-    if kb.is_pressed(key_stop) or (count >= amount and amount != 0):
+    if kb.is_pressed(key_stop) or (counter >= amount and amount != 0):
         break
     time.sleep(0.01)
